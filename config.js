abda5456de6b3ae16418da6677f2558b615329e4
Richard Dimalanta <chardmd.au@gmail.com>
2020-05-19 20:36:17 +1000

    integrated prettier

--- config.js
+++ config.js
@@ -1,45 +1,45 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://jamstackbuild.com',
-  pathPrefix: '/',
-  title: 'Jamstack Build',
-  subtitle: 'Your ultimate source for JAMstack tutorials on the web',
-  copyright: '2020 © All rights reserved.',
-  disqusShortname: '',
+  url: "https://jamstackbuild.com",
+  pathPrefix: "/",
+  title: "Jamstack Build",
+  subtitle: "Your ultimate source for JAMstack tutorials on the web",
+  copyright: "2020 © All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: '',
+  googleAnalyticsId: "",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About',
-      path: '/pages/about'
+      label: "About",
+      path: "/pages/about",
     },
   ],
   author: {
-    name: 'Jamstack Build',
-    photo: '/photo.jpg',
-    bio: 'Your ultimate source for JAMstack tutorials on the web',
+    name: "Jamstack Build",
+    photo: "/photo.jpg",
+    bio: "Your ultimate source for JAMstack tutorials on the web",
     contacts: {
-      email: '',
-      facebook: '',
-      telegram: '',
-      twitter: '#',
-      github: '',
-      rss: '',
-      vkontakte: '',
-      linkedin: '',
-      instagram: '',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "",
+      facebook: "",
+      telegram: "",
+      twitter: "#",
+      github: "",
+      rss: "",
+      vkontakte: "",
+      linkedin: "",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,32 +1,52 @@
 // @flow strict
-import React from 'react';
-import moment from 'moment';
-import { Link } from 'gatsby';
-import type { Edges } from '../../types';
-import styles from './Feed.module.scss';
+import React from "react";
+import moment from "moment";
+import { Link } from "gatsby";
+import type { Edges } from "../../types";
+import styles from "./Feed.module.scss";
 
 type Props = {
-  edges: Edges
+  edges: Edges,
 };
 
 const Feed = ({ edges }: Props) => (
-  <div className={styles['feed']}>
+  <div className={styles["feed"]}>
     {edges.map((edge) => (
-      <div className={styles['feed__item']} key={edge.node.fields.slug}>
-        <div className={styles['feed__item-meta']}>
-          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
+      <div className={styles["feed__item"]} key={edge.node.fields.slug}>
+        <div className={styles["feed__item-meta"]}>
+          <time
+            className={styles["feed__item-meta-time"]}
+            dateTime={moment(edge.node.frontmatter.date).format("MMMM D, YYYY")}
+          >
             By
           </time>
-          <span className={styles['feed__item-meta-divider']} />
-          <span className={styles['feed__item-meta-author']}>
-           <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-author-link']}>{edge.node.frontmatter.author}</Link>
+          <span className={styles["feed__item-meta-divider"]} />
+          <span className={styles["feed__item-meta-author"]}>
+            <Link
+              to={edge.node.fields.categorySlug}
+              className={styles["feed__item-meta-author-link"]}
+            >
+              {edge.node.frontmatter.author}
+            </Link>
           </span>
         </div>
-        <h2 className={styles['feed__item-title']}>
-          <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+        <h2 className={styles["feed__item-title"]}>
+          <Link
+            className={styles["feed__item-title-link"]}
+            to={edge.node.fields.slug}
+          >
+            {edge.node.frontmatter.title}
+          </Link>
         </h2>
-        <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
-        <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read more...</Link>
+        <p className={styles["feed__item-description"]}>
+          {edge.node.frontmatter.description}
+        </p>
+        <Link
+          className={styles["feed__item-readmore"]}
+          to={edge.node.fields.slug}
+        >
+          Read more...
+        </Link>
       </div>
     ))}
   </div>
