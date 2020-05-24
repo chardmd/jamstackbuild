8cc336ebf4d811b2b223deb728173592bfe7e31c
Richard Dimalanta <chardmd.au@gmail.com>
2020-05-25 09:24:11 +1000

    updated author component

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,25 +1,46 @@
 // @flow strict
 import React from "react";
-import { getContactHref } from "../../../utils";
+import { Link, withPrefix } from "gatsby";
 import styles from "./Author.module.scss";
+import { getContactHref } from "../../../utils";
 import { useSiteMetadata } from "../../../hooks";
 
 const Author = () => {
   const { author } = useSiteMetadata();
-
   return (
     <div className={styles["author"]}>
-      <p className={styles["author__bio"]}>
-        {author.bio}.
-        <a
-          className={styles["author__bio-twitter"]}
-          href={getContactHref("twitter", author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Follow me on Twitter
-        </a>
-      </p>
+      <div className={styles["author__header"]}>
+        <Link to="/" className={styles["author__header-photo"]}>
+          <img
+            src={withPrefix(author.photo)}
+            width={60}
+            height={60}
+            alt={author.name}
+          />
+        </Link>
+        <div className={styles["author__header-right"]}>
+          <h4 className={styles["author__header-right-name"]}>
+            <Link
+              className={styles["author__header-right-name-link"]}
+              to="/"
+              rel="author"
+            >
+              {author.name}
+            </Link>
+          </h4>
+          <p
+            className={styles["author__header-right-bio"]}
+            dangerouslySetInnerHTML={{ __html: author.bio }}
+          />
+          <a
+            style={{ fontWeight: 500 }}
+            href={getContactHref("twitter", author.contacts.twitter)}
+            target="_blank"
+          >
+            Follow me on Twitter
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
